f321f36a807df6f7e6b521631d621dee7fd9cebe
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-17 20:08:32 -0400

    3. Арифметические операции. [#389575]. Добавлены дополнительные возможности.

--- src/ru/job4j/Calculator.java
+++ src/ru/job4j/Calculator.java
@@ -8,5 +8,16 @@ public class Calculator
         int two = 2;
         int onePlusTwo = one + two;
         System.out.println(onePlusTwo);
+
+        //Additional variables
+        int four = 4;
+        int five = 5;
+        int six = 6;
+
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimesTwo = four * two;
+
+        System.out.println("sixDivTwo = "+ sixDivTwo + "\n" + "fiveMinusTwo = " + fiveMinusTwo + "\n" + "fourTimesTwo = " + fourTimesTwo);
     }
 }
