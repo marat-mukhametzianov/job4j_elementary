2240ec7a29ac4198bd8940468460ac90296191e4
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-18 23:29:51 -0400

    4.1.2. Глупый бот [#389505]. Work and test classes.

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -1,18 +1,17 @@
 package ru.job4j.condition;
 
-public class SqArea
-{
-    public static double square(int perimeter, int ratio)
-    {
-        double width = perimeter / (2*(ratio + 1));
+public class SqArea {
+    public static double square(int perimeter, int ratio) {
+        double width = perimeter / (2 * (ratio + 1));
         double length = width * ratio;
-        double rsl = width * length;;
+        double rsl = width * length;
         return rsl;
     }
 
-    public static void main(String[] args)
-    {
-        double result1 = SqArea.square(6, 2);
-        System.out.println("Perimeter = 6\ncoefficient = 2\narea = " + result1);
+    public static void main(String[] args) {
+        int p = 1;
+        int r = 2;
+        double result1 = SqArea.square(p, r);
+        System.out.println("Perimeter = " + p + "\ncoefficient = " + r + "\narea = " + result1);
     }
 }
