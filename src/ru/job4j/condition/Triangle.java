9bc0271059fd589b6ab9a24c2708202aa9ed2601
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-20 18:53:39 -0400

    4.3.5. Множественное логическое выражение И [#389525]

---
+++ src/ru/job4j/condition/Triangle.java
@@ -0,0 +1,11 @@
+package ru.job4j.condition;
+
+public class Triangle {
+    public static boolean exists(double ab, double ac, double bc) {
+        return (ab + ac) > bc && (ab + bc) > ac && (ac + bc) > ab;
+    }
+
+    public static boolean doesntExist(double ab, double ac, double bc) {
+        return (ab + ac) <= bc || (ab + bc) <= ac || (ac + bc) <= ab;
+    }
+}
---
+++ test/ru/job4j/condition/TriangleTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.condition;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class TriangleTest {
+
+    @Test
+    public void whenTreeFourFiveThenExists() {
+        boolean result = Triangle.exists(3.0, 4.0, 5.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenTenOneOneThenDoesntExist() {
+        boolean result = Triangle.doesntExist(10.0, 1.0, 1.0);
+        assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenTenFiveFiveThenDoesntExist() {
+        boolean result = Triangle.doesntExist(10.0, 5.0, 5.0);
+        assertThat(result, is(true));
+    }
+}
