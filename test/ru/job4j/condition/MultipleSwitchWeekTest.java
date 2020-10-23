8c113229134c14be588cc2e27c42e6bb7ad68de8
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-22 22:57:06 -0400

    4.6.1. Switch [#389518]

--- test/ru/job4j/condition/MultipleSwitchWeekTest.java
+++ test/ru/job4j/condition/MultipleSwitchWeekTest.java
@@ -14,4 +14,124 @@ public class MultipleSwitchWeekTest {
         int out = new MultipleSwitchWeek().numberOfDay(day);
         Assert.assertEquals(expected, out);
     }
+
+    @Test
+    public void whenRuTuesdayThen2() {
+        String day = "Вторник";
+        int expected = 2;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuWednesdayThen3() {
+        String day = "Среда";
+        int expected = 3;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuThursdayThen4() {
+        String day = "Четверг";
+        int expected = 4;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuFridayThen5() {
+        String day = "Пятница";
+        int expected = 5;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuSaturdayThen6() {
+        String day = "Суббота";
+        int expected = 6;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuSundayThen7() {
+        String day = "Воскресенье";
+        int expected = 7;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenRuAnymoreThenMinus1() {
+        String day = "Слово";
+        int expected = -1;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenMondayThen1() {
+        String day = "Monday";
+        int expected = 1;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenTuesdayThen2() {
+        String day = "Tuesday";
+        int expected = 2;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenWednesdayThen3() {
+        String day = "Wednesday";
+        int expected = 3;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenThursdayThen4() {
+        String day = "Thursday";
+        int expected = 4;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenFridayThen5() {
+        String day = "Friday";
+        int expected = 5;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenSaturdayThen6() {
+        String day = "Saturday";
+        int expected = 6;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenSundayThen7() {
+        String day = "Sunday";
+        int expected = 7;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
+
+    @Test
+    public void whenAnymoreThenMinus1() {
+        String day = "Word";
+        int expected = -1;
+        int out = new MultipleSwitchWeek().numberOfDay(day);
+        Assert.assertEquals(expected, out);
+    }
 }
