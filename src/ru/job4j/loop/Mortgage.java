a84f95b81fa307272ac47cd4690cb7e29abf47c0
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-24 12:26:54 -0400

    5.7. Ипотека [#389621]

---
+++ src/ru/job4j/loop/Mortgage.java
@@ -0,0 +1,14 @@
+package ru.job4j.loop;
+
+public class Mortgage {
+    public static int year(int amount, int salary, double percent) {
+        int year = 0;
+        double leftover = amount;
+
+        while (leftover > 0) {
+            leftover = leftover + leftover * percent/100 - salary;
+            year ++;
+        }
+        return year;
+    }
+}
