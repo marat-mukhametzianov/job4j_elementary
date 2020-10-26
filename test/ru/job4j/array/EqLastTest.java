e8a3874383b6826ce1da626bcda80b75cbbe3110
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-26 19:38:56 -0400

    6.4.1. Сравнить последние элементы двух массивов [#389550]

---
+++ test/ru/job4j/array/EqLastTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class EqLastTest {
+
+    @Test
+    public void whenEqAndDifferentLengths() {
+        int[] left = {1, 2, 3, 7, 3};
+        int[] right = {5, 4, 3};
+        assertThat(EqLast.check(left, right), is(true));
+    }
+
+    @Test
+    public void whenNotEqAndDifferentLengths() {
+        int[] left = {1, 2, 3, 7, 3};
+        int[] right = {5, 0};
+        assertThat(EqLast.check(left, right), is(false));
+    }
+
+    @Test
+    public void whenEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {5, 4, 3};
+        assertThat(EqLast.check(left, right), is(true));
+    }
+
+    @Test
+    public void whenNotEq() {
+        int[] left = {1, 2, 3};
+        int[] right = {3, 3, 4};
+        assertThat(EqLast.check(left, right), is(false));
+    }
+}
