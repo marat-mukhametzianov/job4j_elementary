ab4aea89c7c054f2076f389a6b5b3e7f5c659f5f
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-28 21:55:58 -0400

    6.8. Дефрагментация массива [#389557]

---
+++ src/ru/job4j/array/Defragment.java
@@ -0,0 +1,29 @@
+package ru.job4j.array;
+
+public class Defragment {
+    public static String[] compress(String[] array) {
+        for (int index = 0; index < array.length; index++) {
+            if (array[index] == null) {
+                int emptyPointer = index;
+                for (int subIndex = index + 1; subIndex < array.length; subIndex++) {
+                    if (array[subIndex] != null) {
+                        array[emptyPointer] = array[subIndex];
+                        array[subIndex] = null;
+                        break;
+                    }
+                }
+            }
+            System.out.print(array[index] + " ");
+        }
+        return array;
+    }
+
+    public static void main(String[] args) {
+        String[] input = {"I", null, "wanna", null, "be", null, "compressed"};
+        String[] compressed = compress(input);
+        System.out.println();
+        for (int index = 0; index < compressed.length; index++) {
+            System.out.print(compressed[index] + " ");
+        }
+    }
+}
