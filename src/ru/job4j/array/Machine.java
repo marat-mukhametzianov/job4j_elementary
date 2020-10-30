a7043ec93e94f92192be7d659ae2852983e806fb
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-10-29 21:16:09 -0400

    6.9. Жадный алгоритм. Сдача в кофе машине [#389530]

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -6,19 +6,27 @@ public class Machine {
     private final int[] COINS = {10, 5, 2, 1};
 
     public int[] change(int money, int price) {
-        int[] rsl = new int[100];
-        int delta = money - price;
+        int[] result = new int[100];
+        int emptyCellPointer = 0;
 
+        int number = money - price;
 
+        for (int i = 0; i < COINS.length; i++) {
+            int numberQuantity = number / COINS[i];
+            int leftover = number % COINS[i];
 
-
-        if (delta / 10 > 1) {
-
+            if (numberQuantity >= 1) {
+                for (int counter = 1; counter <= numberQuantity; counter++) {
+                    result[emptyCellPointer] = COINS[i];
+                    emptyCellPointer += 1;
+                }
+                if (leftover != 0) {
+                    number = leftover;
+                } else {
+                    break;
+                }
+            }
         }
-
-
-
-        int size = 0;
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(result, emptyCellPointer);
     }
 }
---
+++ test/ru/job4j/array/MachineTest.java
@@ -0,0 +1,33 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.*;
+
+public class MachineTest {
+
+    @Test
+    public void whenEquals() {
+        Machine machine = new Machine();
+        int[] expected = {};
+        int[] rsl = machine.change(100, 100);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void when50by35() {
+        Machine machine = new Machine();
+        int[] expected = {10, 5};
+        int[] rsl = machine.change(50, 35);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void when100buy23() {
+        Machine machine = new Machine();
+        int[] expected = {10, 10, 10, 10, 10, 10, 10, 5, 2};
+        int[] rsl = machine.change(100, 23);
+        assertThat(rsl, is(expected));
+    }
+}
