114b2ea2ee2bbad2b51d635b9b4f380b871fd74c
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-01 00:00:38 -0400

    6.9. Жадный алгоритм. Сдача в кофе машине [#389530]. Добавлен второй метод и тесты для него.

--- src/ru/job4j/array/Machine.java
+++ src/ru/job4j/array/Machine.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 public class Machine {
     private final int[] COINS = {10, 5, 2, 1};
 
-    public int[] change(int money, int price) {
+    public int[] changeOnce(int money, int price) {
         int[] result = new int[100];
         int emptyCellPointer = 0;
 
@@ -25,6 +25,25 @@ public class Machine {
                 } else {
                     break;
                 }
+            }
+        }
+        return Arrays.copyOf(result, emptyCellPointer);
+    }
+
+    public int[] change(int money, int price) {
+        int[] result = new int[100];
+        byte emptyCellPointer = 0;
+        int delta = money - price;
+
+        for (byte i = 0; i < COINS.length; i++) {
+
+            while (delta >= COINS[i] ) {
+                result[emptyCellPointer] = COINS[i];
+                delta -= COINS[i];
+                emptyCellPointer++;
+            }
+            if (delta == 0) {
+                break;
             }
         }
         return Arrays.copyOf(result, emptyCellPointer);
--- test/ru/job4j/array/MachineTest.java
+++ test/ru/job4j/array/MachineTest.java
@@ -8,6 +8,30 @@ import static org.junit.Assert.*;
 public class MachineTest {
 
     @Test
+    public void whenOnceEquals() {
+        Machine machine = new Machine();
+        int[] expected = {};
+        int[] rsl = machine.changeOnce(100, 100);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenOnce50by35() {
+        Machine machine = new Machine();
+        int[] expected = {10, 5};
+        int[] rsl = machine.changeOnce(50, 35);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
+    public void whenOnce100buy23() {
+        Machine machine = new Machine();
+        int[] expected = {10, 10, 10, 10, 10, 10, 10, 5, 2};
+        int[] rsl = machine.changeOnce(100, 23);
+        assertThat(rsl, is(expected));
+    }
+
+    @Test
     public void whenEquals() {
         Machine machine = new Machine();
         int[] expected = {};
