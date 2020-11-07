1ae07a63b8feefce90456bec35824ea7e41999b9
marat-mukhametzianov <marat.mukhametzianov@yahoo.com>
2020-11-07 18:05:44 -0500

    3. Перегрузить метод max для трех чисел [#389721]

--- test/ru/job4j/condition/MaxTest.java
+++ test/ru/job4j/condition/MaxTest.java
@@ -7,29 +7,50 @@ import static org.junit.Assert.*;
 public class MaxTest {
 
     @Test
-    public void whenLeft1Right3Then3() {
-        int left = 1;
-        int right = 3;
+    public void whenFirst1Second3Then3() {
+        int first = 1;
+        int second = 3;
         int expected = 3;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
         assertThat(out, is(expected));
     }
 
     @Test
-    public void whenLeft3Right1Then3() {
-        int left = 3;
-        int right = 1;
+    public void whenFirst3Secondt1Then3() {
+        int first = 3;
+        int second = 1;
         int expected = 3;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
         assertThat(out, is(expected));
     }
 
     @Test
-    public void whenLeft2Right2Then2() {
-        int left = 2;
-        int right = 2;
+    public void whenFirst2Second2Then2() {
+        int first = 2;
+        int second = 2;
         int expected = 2;
-        int out = Max.max(left, right);
+        int out = Max.max(first, second);
+        assertThat(out, is(expected));
+    }
+
+    @Test
+    public void whenFirst5Second3Third7Then7() {
+        int first = 5;
+        int second = 3;
+        int third = 7;
+        int expected = 7;
+        int out = Max.max(first, second, third);
+        assertThat(out, is(expected));
+    }
+
+    @Test
+    public void whenFirst5Second3Third7Fourth11Then11() {
+        int first = 5;
+        int second = 3;
+        int third = 7;
+        int fourth = 11;
+        int expected = 11;
+        int out = Max.max(first, second, third, fourth);
         assertThat(out, is(expected));
     }
 }
